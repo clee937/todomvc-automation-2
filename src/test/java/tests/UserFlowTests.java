c2efd53ead46c7c70c836990723705653371e49e
cheryl lee <75495899+clee937@users.noreply.github.com>
2026-07-27 17:55:17 +0100

    refactored to extend BaseTest for shared test setup

--- src/test/java/tests/UserFlowTests.java
+++ src/test/java/tests/UserFlowTests.java
@@ -1,31 +1,14 @@
 package tests;
 
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import pages.TodoMVCReactPage;
 
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-public class TodoMVCUserFlowTests {
-    private WebDriver driver;
-    private TodoMVCReactPage todoPage;
-
-    @BeforeEach
-    void setUp() {
-        driver = new ChromeDriver();
-        todoPage = new TodoMVCReactPage(driver);
-        todoPage.open();
-    }
-
-    @AfterEach
-    void tearDown() {
-        driver.quit();
-    }
+@DisplayName("User Flows")
+public class UserFlowTests extends BaseTest {
 
     @Test
     void shouldCompleteTodoLifeCycle() {
